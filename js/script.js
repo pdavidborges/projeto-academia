5736f108152c4d91cf32db5734368f609b560ea8
David Borges <112510561+pdavidborges@users.noreply.github.com>
2024-03-05 20:33:12 -0300

    Atualização do Popup

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 var btnMenu = document.querySelector('.btn-menu');
 var menu = document.querySelector('.menu');
 
+
+
 //console.log(btnMenu);
 //console.log(menu);
 
@@ -19,17 +21,24 @@ function boasVindas(){
 btnMenu.addEventListener('click',boasVindas);
 */
 
-
 //Ao clicar no btnMenu, chama uma função anônima 
-btnMenu.addEventListener('click', function(){
-    
+btnMenu.addEventListener('click', function(){    
     //add ou remove a classe menu-open
     menu.classList.toggle('menu-open');
     btnMenu.classList.toggle('x');
-
 })
 
 menu.addEventListener('click', function(){
     menu.classList.remove('menu-open');
     btnMenu.classList.remove('x');    
 })
+
+var btnPopup = document.querySelector('.close');
+var bgPopup = document.querySelector('.bg-popup');
+
+//Fechar a estrutura do popup ao clicar no botão X
+btnPopup.addEventListener('click',function(){
+    //bgPopup.classList.add('bg-popup-none');
+    bgPopup.style.display = 'none';
+})
+
